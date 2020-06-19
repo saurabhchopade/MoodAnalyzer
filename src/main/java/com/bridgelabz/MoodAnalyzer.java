88699ae2ae5faaf46ad51f2de7b2b27ad97097ea
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-19 19:15:39 +0530

    ADDED Mood Testing feature

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+
+    public String analyzeMood(String messege) {
+        if (messege.contains("sad"))
+        {
+            return "SAD";
+        }
+        else{
+            return "SAD";
+        }
+
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+
+    @Test
+    public void testAnalyzeMood() {
+        MoodAnalyzer moodAnalyzer= new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("This is sad Method");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void newMoodAnalyzer() {
+        MoodAnalyzer moodAnalyzer= new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("This is happy Messege");
+        Assert.assertEquals("HAPPY",mood);
+
+    }
+}
